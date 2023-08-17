736767b99a3936fb04ffc1dfdbdf5d3781957c14
ZachMommaerts <zmommaerts@gmail.com>
2023-08-17 14:29:03 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Zach Mommaerts on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
