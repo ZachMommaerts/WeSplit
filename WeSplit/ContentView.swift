31c28e8eb8fe0868032fe168afd6ee33fdc305a3
ZachMommaerts <zmommaerts@gmail.com>
2023-09-07 11:41:02 -0500

    - Create conditional modifier to change total to red if tip is 0%

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -60,6 +60,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundColor(tipPercentage == 0 ? .red : .black)
                 } header: {
                     Text("Total amount")
                 }
